42641ea818d61f51363e5b3ae6c71d351e13bd59
Fabien Sanglard <fabien.sanglard@gmail.com>
2012-03-03 12:07:53 -0800

    Added comment for understanding memory segment usage.

--- parts.cpp
+++ parts.cpp
@@ -9,8 +9,9 @@
 */
 
 /*
-	It seems that:
+	MEMLIST_PART_VIDEO1 and MEMLIST_PART_VIDEO2 are used to store polygons. 
 
+	It seems that:
 	- MEMLIST_PART_VIDEO1 contains the cinematic polygons.
 	- MEMLIST_PART_VIDEO2 contains the polygons for player and enemies animations.
 
@@ -18,14 +19,16 @@
 
 */
 const uint16_t memListParts[GAME_NUM_PARTS][4] = {
-	{ 0x14, 0x15, 0x16, 0x00 }, // protection screens
-	{ 0x17, 0x18, 0x19, 0x00 }, // introduction cinematic
-	{ 0x1A, 0x1B, 0x1C, 0x11 },
-	{ 0x1D, 0x1E, 0x1F, 0x11 },
-	{ 0x20, 0x21, 0x22, 0x11 },
-	{ 0x23, 0x24, 0x25, 0x00 }, // battlechar cinematic
-	{ 0x26, 0x27, 0x28, 0x11 },
-	{ 0x29, 0x2A, 0x2B, 0x11 },
-	{ 0x7D, 0x7E, 0x7F, 0x00 },
-	{ 0x7D, 0x7E, 0x7F, 0x00 }  // password screen
+
+//MEMLIST_PART_PALETTE   MEMLIST_PART_CODE   MEMLIST_PART_VIDEO1   MEMLIST_PART_VIDEO2
+	{ 0x14,                    0x15,                0x16,                0x00 }, // protection screens
+	{ 0x17,                    0x18,                0x19,                0x00 }, // introduction cinematic
+	{ 0x1A,                    0x1B,                0x1C,                0x11 },
+	{ 0x1D,                    0x1E,                0x1F,                0x11 },
+	{ 0x20,                    0x21,                0x22,                0x11 },
+	{ 0x23,                    0x24,                0x25,                0x00 }, // battlechar cinematic
+	{ 0x26,                    0x27,                0x28,                0x11 },
+	{ 0x29,                    0x2A,                0x2B,                0x11 },
+	{ 0x7D,                    0x7E,                0x7F,                0x00 },
+	{ 0x7D,                    0x7E,                0x7F,                0x00 }  // password screen
 };
