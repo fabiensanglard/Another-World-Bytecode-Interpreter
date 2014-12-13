a704d665445fd2d9170f3695f8ceaa28e57420f0
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2014-12-13 16:28:50 -0200

    a comment on the meaning of a specific set of resources

--- parts.cpp
+++ parts.cpp
@@ -23,7 +23,7 @@ const uint16_t memListParts[GAME_NUM_PARTS][4] = {
 //MEMLIST_PART_PALETTE   MEMLIST_PART_CODE   MEMLIST_PART_VIDEO1   MEMLIST_PART_VIDEO2
 	{ 0x14,                    0x15,                0x16,                0x00 }, // protection screens
 	{ 0x17,                    0x18,                0x19,                0x00 }, // introduction cinematic
-	{ 0x1A,                    0x1B,                0x1C,                0x11 },
+	{ 0x1A,                    0x1B,                0x1C,                0x11 }, //lake initial stage + beast hunt
 	{ 0x1D,                    0x1E,                0x1F,                0x11 },
 	{ 0x20,                    0x21,                0x22,                0x11 },
 	{ 0x23,                    0x24,                0x25,                0x00 }, // battlechar cinematic
